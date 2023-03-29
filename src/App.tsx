539a6257ffdb9b68889ce0e936a2c489a3c30968
Sahinugur <ugurlao@gmail.com>
2023-03-29 13:26:13 +0200

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Navbar from "@/scenes/navbar"
+import { useState } from "react"
+
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("home")
   return (
-    <div className="App">
-     App
+    <div className="App bg-gray-20">
+        <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
